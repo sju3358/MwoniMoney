b5b0add1d327ba745a233d789284cdaa4d8ceee0
sju3358 <tjdfkr011.dev@gmail.com>
2023-09-13 15:56:42 +0900

    feat : 퀴즈 엔티티 설계

--- wannabee/src/main/java/com/ntt/wannabee/domain/quiz/entity/QuizHistoryKey.java
+++ wannabee/src/main/java/com/ntt/wannabee/domain/quiz/entity/QuizHistoryKey.java
@@ -1,4 +1,23 @@
 package com.ntt.wannabee.domain.quiz.entity;
 
-public class QuizHistoryKey {
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class QuizHistoryKey implements Serializable {
+
+	@Column(name = "quiz_idx")
+	private Long quizIdx;
+
+	@Column(name = "member_uuid")
+	private String memberUUID;
 }
