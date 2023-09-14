8090c2a67fee67e5e9a6b0067419d6a4ede83304
조성락 <tjdfkr011@naver.com>
2023-09-14 17:47:23 +0900

    Revert "[S09P22B310-071] feat : @Service 어노테이션 인터페이스만 붙임"
    This reverts commit 257d0aecdb58cbfe91ee3447e01a9d297d8f9dd0

--- backend/src/main/java/com/ntt/mwonimoney/domain/game/service/BalanceGameHistoryService.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/game/service/BalanceGameHistoryService.java
@@ -3,10 +3,12 @@ package com.ntt.mwonimoney.domain.game.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ntt.mwonimoney.domain.game.model.dto.BalanceGameHistoryDto;
 
 @Service
+@Transactional(readOnly = true)
 public interface BalanceGameHistoryService {
 	List<BalanceGameHistoryDto> getUserBalanceGameHistories(Long memberIdx);
 }
