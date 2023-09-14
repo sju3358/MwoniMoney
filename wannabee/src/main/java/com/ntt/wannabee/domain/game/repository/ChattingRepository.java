cb57bff696d907afe2305db97c1f78131a1023ae
tjdfkr011 <tjdfkr011@naver.com>
2023-09-14 15:01:07 +0900

    [S09P22B310-147] feat : 채팅 서비스 구현 완료

--- wannabee/src/main/java/com/ntt/wannabee/domain/game/repository/ChattingRepository.java
+++ wannabee/src/main/java/com/ntt/wannabee/domain/game/repository/ChattingRepository.java
@@ -1,11 +1,16 @@
 package com.ntt.wannabee.domain.game.repository;
 
-public interface ChattingRepository extends ReactiveMongoRepository<Chat, String> {
-	//    @Tailable // 커서를 안닫고 계속 유지한다.
-	//    @Query("{sender: ?0, receiver: ?1}")
-	//    Flux<ChatResponseDTO> mFindBySender(String sender, String receiver); // Flux(흐름) response를 유지하면서 데이터를 계속 흘려보내기@Tailable // 커서를 안닫고 계속 유지한다.
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
 
+import com.ntt.wannabee.domain.game.entity.Chat;
+
+import reactor.core.publisher.Flux;
+
+public interface ChattingRepository extends ReactiveMongoRepository<Chat, String> {
+	
 	@Tailable // 커서를 안닫고 계속 유지한다.
-	@Query("{bubbleNumber: ?0}")
-	Flux<Chat> mFindByBubbleNumber(Long bubbleNumber);
+	@Query("{balanceGameIdx: ?0}")
+	Flux<Chat> mFindByBalanceGameIdx(Long balanceGameIdx);
 }
