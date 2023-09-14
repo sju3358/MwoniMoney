abe3219890676df45949c47c6a1359c36d005be8
tjdfkr011 <tjdfkr011@naver.com>
2023-09-14 17:48:45 +0900

    [S09P22B310-071] feat : @Service 어노테이션 위치 변경

--- backend/src/main/java/com/ntt/mwonimoney/domain/game/service/WordCloudService.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/game/service/WordCloudService.java
@@ -1,13 +1,10 @@
 package com.ntt.mwonimoney.domain.game.service;
 
-import org.springframework.stereotype.Service;
-
 import com.ntt.mwonimoney.domain.game.entity.Word;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@Service
 public interface WordCloudService {
 
 	Flux<Word> getWordCloudData(Long balanceGameIdx);
