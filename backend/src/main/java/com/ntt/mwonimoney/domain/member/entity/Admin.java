dfe00f43ce121e663075e84d436cfdd5f2b1e16f
tjdfkr011 <tjdfkr011@naver.com>
2023-09-14 16:01:12 +0900

    [S09P22B310-79] feat : 멤버 엔티티 설계 변경

--- backend/src/main/java/com/ntt/mwonimoney/domain/member/entity/Admin.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/member/entity/Admin.java
@@ -1,18 +1,28 @@
 package com.ntt.mwonimoney.domain.member.entity;
 
 import com.ntt.mwonimoney.domain.member.model.dto.AdminDto;
+import com.ntt.mwonimoney.domain.member.model.vo.MemberRole;
+import com.ntt.mwonimoney.domain.member.model.vo.SmallAccount;
+import com.ntt.mwonimoney.domain.member.model.vo.SocialProvider;
 
 import jakarta.persistence.Entity;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Entity
-@SuperBuilder
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Admin extends Member {
+
+	@Builder
+	public Admin(byte status, String name, String nickname, SocialProvider socialProvider, String socialId){
+
+		super(status, name, nickname, "", socialProvider, socialId, MemberRole.ADMIN);
+
+	}
 
 	public AdminDto convertToDto() {
 		return AdminDto.builder()
