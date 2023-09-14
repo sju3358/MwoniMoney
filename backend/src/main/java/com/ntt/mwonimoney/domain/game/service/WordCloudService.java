1fbb7378d2ea7aac09f98318b0cb7728467b0c88
tjdfkr011 <tjdfkr011@naver.com>
2023-09-14 16:39:26 +0900

    [S09P22B310-147] feat : 워드클라우드 기능 구현

--- backend/src/main/java/com/ntt/mwonimoney/domain/game/service/WordCloudService.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/game/service/WordCloudService.java
@@ -1,4 +1,35 @@
 package com.ntt.mwonimoney.domain.game.service;
 
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+
+import com.ntt.mwonimoney.domain.game.entity.Chat;
+import com.ntt.mwonimoney.domain.game.entity.Word;
+import com.ntt.mwonimoney.domain.game.repository.WordCloudRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class WordCloudService {
+
+	private final WordCloudRepository wordCloudRepository;
+
+	public Flux<Word> getWordCloudData(Long balanceGameIdx) {
+		return wordCloudRepository.mFindByBalanceGameIdx(balanceGameIdx)
+			.subscribeOn(Schedulers.boundedElastic());
+	}
+
+	public Mono<Word> addWord(Word word, Long balanceGameIdx) {
+
+		word.setBalanceGameIdx(balanceGameIdx);
+
+		return wordCloudRepository.save(word);
+	}
 }
