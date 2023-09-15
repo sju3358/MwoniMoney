02b3e30486fd29a899a62c378fcf071ca810b0c7
tjdfkr011 <tjdfkr011@naver.com>
2023-09-15 09:30:21 +0900

    [S09P22B310-085] feat : 밸런스게임 히스토리 서비스 구현

--- backend/src/main/java/com/ntt/mwonimoney/domain/game/service/BalanceGameHistoryService.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/game/service/BalanceGameHistoryService.java
@@ -7,5 +7,5 @@ import com.ntt.mwonimoney.domain.game.model.dto.BalanceGameHistoryDto;
 public interface BalanceGameHistoryService {
 	List<BalanceGameHistoryDto> getUserBalanceGameHistories(Long memberIdx);
 
-	void selectBalanceGame(Long balanceGameIdx, Long memberUUID, byte selectAnswer);
+	void selectBalanceGame(Long balanceGameIdx, Long MemberIdx, byte selectAnswer);
 }
