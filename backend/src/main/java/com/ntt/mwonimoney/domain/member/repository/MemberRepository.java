8c47ab26935088dbb1bfcc50b583f65156ec4f41
sju3358 <tjdfkr011.dev@gmail.com>
2023-09-15 09:25:36 +0900

    [S09P22B310-083] feat : 멤버 레포지토리 임시 저장

--- backend/src/main/java/com/ntt/mwonimoney/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/member/repository/MemberRepository.java
@@ -1,9 +1,11 @@
 package com.ntt.mwonimoney.domain.member.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ntt.mwonimoney.domain.member.entity.Member;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
-
+public interface MemberRepository extends JpaRepository<Member, Long> {
+	Optional<Member> findMemberByIdx(Long memberIdx);
 }
