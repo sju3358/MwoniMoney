5ca8822073224b422f2e5178f4ba23fe9c5d5f4a
Jaei <dobbykim0320@gmail.com>
2023-09-17 21:26:20 +0900

    [S09P22B310-142] Feat : Footer 만들기

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import { GlobalStyles } from "./Styles/GlobalStyle";
 import { Routes, Route } from "react-router-dom";
+import React from "react";
 import Main from "./Main";
 import StartPage1 from "../src/pages/StartPage/StartPage1";
-import { GlobalStyles } from "./Styles/GlobalStyle";
+import ChallengeMainParent from "./pages/Parents/Challenge/ChallengeMain";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/StartPage1" element={<StartPage1 />} />
+        <Route path="/ChallengeMainParent" element={<ChallengeMainParent />} />
       </Routes>
     </>
   );
---
+++ frontend-web/src/pages/Parents/Challenge/ChallengeMain.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Footer from "../../../components/footer";
+
+function ChallengeMainParent() {
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
+}
+
+export default ChallengeMainParent;
