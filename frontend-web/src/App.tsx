3cf6f8c6b309a054c2636c2e23ed3ae71970a7ab
taekto <taekputer@gmail.com>
2023-09-17 21:22:18 +0900

    [S09P22B310-114] Design : 디자인 변경

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Main from "./Main";
-import StartPage1 from "../src/pages/StartPage/StartPage1";
+import MainPage from "../src/pages/MainPage";
+import StartPage1 from "../src/pages/StartPage/StartPage1"
+import StartPage2 from "../src/pages/StartPage/StartPage2"
+import StartPage3 from "../src/pages/StartPage/StartPage3"
+import StartPage4 from "../src/pages/StartPage/StartPage4"
+import StartPage5 from "../src/pages/StartPage/StartPage5"
+
 import { GlobalStyles } from "./Styles/GlobalStyle";
 
 function App() {
@@ -10,7 +16,12 @@ function App() {
       <GlobalStyles />
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/MainPage" element={<MainPage />} />
         <Route path="/StartPage1" element={<StartPage1 />} />
+        <Route path="/StartPage2" element={<StartPage2 />} />
+        <Route path="/StartPage3" element={<StartPage3 />} />
+        <Route path="/StartPage4" element={<StartPage4 />} />
+        <Route path="/StartPage5" element={<StartPage5 />} />
       </Routes>
     </>
   );
