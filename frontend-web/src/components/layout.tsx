f56490201ff84253920f7dac589fb01cc8c5e692
박기택 <taekputer@gmail.com>
2023-09-18 14:31:24 +0900

    [S09P22B310-000] config : react 설정

--- frontend-web/src/components/layout.tsx
+++ frontend-web/src/components/layout.tsx
@@ -3,20 +3,12 @@ import styled from "styled-components";
 import Footer from "./footer";
 import Header from "./header";
 
-const Main = styled.div`
-  width: 100vw;
-  height: 80vh;
-`;
 
-interface LayoutProps {
-  MainContents: React.ReactNode;
-}
 
-function Layout(props: LayoutProps) {
+function Layout() {
   return (
     <>
       <Header />
-      <Main>{props.MainContents}</Main>
       <Footer />
     </>
   );
