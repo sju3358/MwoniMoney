26cdc512923db0278f9c78e055b8d8a8ac1e553a
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-09-18 09:22:06 +0900

    [S09P22B310-154] feat : rChallenge @Column 추가

--- backend/src/main/java/com/ntt/mwonimoney/domain/challenge/entity/Challenge.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/challenge/entity/Challenge.java
@@ -1,7 +1,9 @@
 package com.ntt.mwonimoney.domain.challenge.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,11 +14,14 @@ import lombok.NoArgsConstructor;
 @Getter
 public class Challenge {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "challenge_idx")
 	private Integer challengeIdx;
 
+	@Column(name = "challenge_title")
 	private String title;
 
+	@Column(name = "challenge_category")
 	private String category;
 
 	//생성자
