d4028f93705594ecb7e608b079f22862fe8ecf03
윤민재 <w124564@naver.com>
2023-09-19 21:16:02 +0900

    [S09P22B310-182] feat : OAuth 개발

--- backend/src/main/java/com/ntt/mwonimoney/domain/oauth/service/CustomOAuth2UserService.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/oauth/service/CustomOAuth2UserService.java
@@ -1,12 +1,20 @@
 package com.ntt.mwonimoney.domain.oauth.service;
 
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import com.ntt.mwonimoney.domain.member.entity.Member;
+import com.ntt.mwonimoney.domain.member.entity.Parent;
+import com.ntt.mwonimoney.domain.member.model.vo.SocialProvider;
 import com.ntt.mwonimoney.domain.member.repository.MemberRepository;
+import com.ntt.mwonimoney.domain.oauth.exception.OAuthProviderMissMatchException;
+import com.ntt.mwonimoney.domain.oauth.info.OAuth2MemberInfo;
+import com.ntt.mwonimoney.domain.oauth.info.OAuth2UserInfoFactory;
+import com.ntt.mwonimoney.domain.oauth.model.vo.MemberPrincipal;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,48 +30,41 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 	@Override
 	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 		OAuth2User user = super.loadUser(userRequest);
-		return user;
 
-		// try {
-		// 	return this.process(userRequest, user);
-		// } catch (Exception ex) {
-		// 	log.error("CustomOAuth2UserService loadUser Error: {} ", ex.getMessage());
-		// 	throw new InternalAuthenticationServiceException(ex.getMessage(), ex.getCause());
-		// }
+		try {
+			return this.process(userRequest, user);
+		} catch (Exception ex) {
+			log.error("CustomOAuth2UserService loadUser Error: {} ", ex.getMessage());
+			throw new InternalAuthenticationServiceException(ex.getMessage(), ex.getCause());
+		}
 	}
 
-	// private OAuth2User process(OAuth2UserRequest userRequest, OAuth2User user) {
-	// 	SocialProvider providerType = SocialProvider.valueOf(
-	// 		userRequest.getClientRegistration().getRegistrationId().toUpperCase());
-	//
-	// 	OAuth2UserInfo userInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(providerType, user.getAttributes());
-	// 	log.info(userInfo.getId());
-	//
-	// 	Users savedUser = memberRepository.findBySocialId(userInfo.getId());
-	//
-	// 	if (savedUser != null) {
-	// 		if (providerType != savedUser.getProviderType()) {
-	// 			log.error("CustomOAuth2UserService process Error: 다른 소셜에서 가입된 이메일 입니다. 해당 소셜 로그인을 이용해 주세요.");
-	// 			throw new OAuthProviderMissMatchException(ALREADY_SIGNED_UP_SOCIAL);
-	// 		}
-	// 		// 여기에 기기 토큰 저장
-	// 		updateUser(savedUser, userInfo);
-	// 	} else {
-	// 		savedUser = createUser(userInfo, providerType);
-	// 	}
-	//
-	// 	return UserPrincipal.create(savedUser, user.getAttributes());
-	// }
+	private OAuth2User process(OAuth2UserRequest userRequest, OAuth2User user) {
+		SocialProvider socialProvider = SocialProvider.valueOf(
+			userRequest.getClientRegistration().getRegistrationId().toUpperCase());
 
-	// private Member createUser(OAuth2UserInfo userInfo, ProviderType providerType) {
-	// 	Member user = Member.builder()
-	// 		.socialId(userInfo.getId())
-	// 		.email(userInfo.getEmail())
-	// 		.name(userInfo.getName())
-	// 		.providerType(providerType)
-	// 		.roles(Collections.singletonList(RoleType.ROLE_USER.name()))
-	// 		.build();
-	// 	return memberRepository.saveAndFlush(user);
-	// }
+		OAuth2MemberInfo userInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(socialProvider, user.getAttributes());
+		log.info(userInfo.getId());
 
+		Member savedMember = memberRepository.findMemberBySocialId(userInfo.getId())
+			.orElseGet(() -> createMember(userInfo, socialProvider));
+
+		if (socialProvider != savedMember.getSocialProvider()) {
+			log.error("CustomOAuth2UserService process Error: 다른 소셜에서 가입된 이메일 입니다. 해당 소셜 로그인을 이용해 주세요.");
+			throw new OAuthProviderMissMatchException(ALREADY_SIGNED_UP_SOCIAL);
+		}
+
+		return MemberPrincipal.create(savedMember, user.getAttributes());
+	}
+
+	private Member createMember(OAuth2MemberInfo userInfo, SocialProvider socialProvider) {
+		Member member = Parent.builder()
+			.socialId(userInfo.getId())
+			.name(userInfo.getName())
+			.socialProvider(socialProvider)
+			.status((byte)1)
+			.build();
+
+		return memberRepository.saveAndFlush(member);
+	}
 }
