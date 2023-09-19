530b76b5982285b9d7bb7df331e433ec2ee1130d
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-09-19 23:30:40 +0900

    [S09P22B310-195] Design: 퀴즈 css

--- frontend-web/src/pages/ChildrenPage.tsx
+++ frontend-web/src/pages/ChildrenPage.tsx
@@ -1,7 +1,59 @@
 import React from "react";
+import {
+  MainContainer,
+  Text,
+  TextBold_SP,
+  Text_GR,
+  TextContainer,
+  TextEmojiBox,
+  TextMentBox,
+  Emoji,
+  BalanceContainer,
+  QuizContainer,
+  ChallengeContainer,
+} from "../components/Children/Main";
+
+//컴포넌트
+import Balance from "../components/Children/Balance";
+import Quiz from "../components/Children/Quiz/Quiz";
+import Challenge from "../components/Children/Challenge";
+
+//이미지
+import Coin from "../assests/image/main/Coin.png";
 
 function ChildrenPage() {
-  return <div>자식 페이지입니다. </div>;
+  const asset = "100,000원";
+  const debt = "10,000원";
+  return (
+    <MainContainer>
+      <TextContainer>
+        <TextMentBox>
+          <Text>현재</Text>
+          <Text>
+            <TextBold_SP>{asset}</TextBold_SP>을 가지고 있고
+          </Text>
+          <Text>
+            <TextBold_SP>{debt}</TextBold_SP>의 빚을 가지고
+          </Text>
+          <Text>있어요!</Text>
+          <Text_GR>퀴즈를 맞추거나</Text_GR>
+          <Text_GR>챌린지에 도전해서 용돈을 얻어보세요!</Text_GR>
+        </TextMentBox>
+        <TextEmojiBox>
+          <Emoji url={`${Coin}`} width="80%" height="80%" />
+        </TextEmojiBox>
+      </TextContainer>
+      <BalanceContainer height="40%">
+        <Balance />
+      </BalanceContainer>
+      <QuizContainer height="30%">
+        <Quiz />
+      </QuizContainer>
+      <ChallengeContainer height="60%">
+        <Challenge />
+      </ChallengeContainer>
+    </MainContainer>
+  );
 }
 
 export default ChildrenPage;
