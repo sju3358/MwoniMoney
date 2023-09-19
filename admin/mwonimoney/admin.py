6ebc4acbb5da39756e0ce03fece64177345206af
aldms8960 <45choi22@gmail.com>
2023-09-19 15:07:00 +0900

    Fix: docker build 에러 수정

---
+++ admin/mwonimoney/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from mwonimoney.models import Test
+
+admin.site.register(Test)
---
+++ admin/mwonimoney/my_settings.py
@@ -0,0 +1,12 @@
+mySECRET_KEY = 'django-insecure-vce0jr_sl(8t84axzii-zi+!yigles=up!9et52=h02yv4a0k1'
+
+myDATABASES = {
+	'default' : {
+					'ENGINE' : 'django.db.backends.mysql', # 벡엔드 엔진
+					'NAME' : 'wannabedb', # 'mysql'의 이름을 가진 데이터베이스
+					'USER' : 'wannabe', # 계정
+					'PASSWORD' : 'i9b310p_kplyjc', #rootpassword로 지정할 숫자(6번에 나와있음)
+					'HOST' : 'j9b310.p.ssafy.io',
+					'PORT' : '50000'
+		}
+}
