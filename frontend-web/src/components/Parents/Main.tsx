ca3987bff5cc6e5ead157afe1d84283db533bea8
Jaei <dobbykim0320@gmail.com>
2023-09-19 14:52:44 +0900

    [S09P22B310-188] Design : 부모메인 용돈컴포넌트 작업

--- frontend-web/src/components/Parents/Main.tsx
+++ frontend-web/src/components/Parents/Main.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import Money from "../../assests/image/Money.png";
 
 export const MainContainer = styled.div`
-  border: 1px solid black;
+  // border: 1px solid black;
   width: 100%;
   height: 100%;
   box-sizing: border-box;
@@ -21,11 +21,12 @@ export const ChildrenList = styled.div`
 }
 
 export const TextContainer = styled.div`
-  border: 1px solid black;
+  // border: 1px solid black;
   width: 100%;
   height: 25%;
   box-sizing: border-box;
   display: flex;
+  padding: 5%;
 `;
 export const TextMentBox = styled.div`
   // border: 1px solid red;
@@ -69,41 +70,36 @@ export const Emoji = styled.div`
 `;
 
 {
-  /* 정기용돈 넣는 칸 */
+  /* 용돈 넣는 칸 */
 }
 
-export const AllowanveContainer = styled.div`
-  border: 1px solid black;
+interface AllowanceProps {
+  height: string; // 'String' -> 'string'
+}
+
+export const AllowanveContainer = styled.div<AllowanceProps>`
+  // border: 1px solid black;
   width: 100%;
-  height: 50%;
+  height: ${(props) => props.height};
   box-sizing: border-box;
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-bottom: 5%;
+  //
 `;
-
-export const WhiteBox = styled.div`
-  // border: 1px solid black;
-  border-radius: 8px;
-  width: 90%;
-  height: 90%;
-  background-color: #ffffff;
-`;
-
-{
-  /* 특별용돈 넣는 칸 */
-}
 
 {
   /* QR 넣는 칸 */
 }
 
 export const QrContainer = styled.div`
-  border: 1px solid black;
+  // border: 1px solid black;
   width: 100%;
-  height: 30%;
+  height: 35%;
   box-sizing: border-box;
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-bottom: 5%;
 `;
--- frontend-web/src/pages/MainPage.tsx
+++ frontend-web/src/pages/MainPage.tsx
@@ -11,8 +11,9 @@ const Logo = styled.div`
   /* 로고 이미지 스타일 설정 */
   width: 61.67%; /* 가로 너비를 화면 너비의 61.67%로 설정 */
   height: 30.44%; /* 높이를 화면 높이의 28.44%로 설정 */
-  background-image: url(${process.env.PUBLIC_URL}/images/StartPage/mainlogo.png);
-  background-size: 100% 100%;  
+  background-image: url(${process.env
+    .PUBLIC_URL}/images/StartPage/mainlogo.png);
+  background-size: 100% 100%;
   position: absolute;
   left: 19.31vw; /* 왼쪽으로 19.31%만큼 이동 */
   top: 24.13vh; /* 위로 28.13%만큼 이동 */
@@ -22,7 +23,8 @@ const KaKaoLogin = styled.div`
   /* 카카오 로그인 이미지 스타일 설정 */
   width: 84.72vw; /* 가로 너비를 화면 너비의 84.72%로 설정 */
   height: 7.81vh; /* 높이를 화면 높이의 7.81%로 설정 */
-  background-image: url(${process.env.PUBLIC_URL}/images/StartPage/KaKaoLogin.png);
+  background-image: url(${process.env
+    .PUBLIC_URL}/images/StartPage/KaKaoLogin.png);
   background-size: 100% 100%;
   position: absolute;
   left: 7.78vw; /* 왼쪽으로 7.78%만큼 이동 */
@@ -33,7 +35,8 @@ const GoogleLogin = styled.div`
   /* 구글 로그인 이미지 스타일 설정 */
   width: 84.72vw; /* 가로 너비를 화면 너비의 84.72%로 설정 */
   height: 7.81vh; /* 높이를 화면 높이의 7.81%로 설정 */
-  background-image: url(${process.env.PUBLIC_URL}/images/StartPage/GoogleLogin.png);
+  background-image: url(${process.env
+    .PUBLIC_URL}/images/StartPage/GoogleLogin.png);
   background-size: 100% 100%;
   position: absolute;
   left: 7.78vw; /* 왼쪽으로 7.78%만큼 이동 */
@@ -43,9 +46,9 @@ const GoogleLogin = styled.div`
 function MyComponent() {
   return (
     <Container>
-      <Logo/>
-      <KaKaoLogin/>
-      <GoogleLogin/>
+      <Logo />
+      <KaKaoLogin />
+      <GoogleLogin />
     </Container>
   );
 }
