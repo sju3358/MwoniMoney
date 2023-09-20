b64bba331346859190b8fd1b9d8ad89132eb036b
Jaei <dobbykim0320@gmail.com>
2023-09-20 17:39:03 +0900

    [S09P22B310-230] Design : 부모은행 부모화면 작업1

--- frontend-web/src/components/Common/AboutText.tsx
+++ frontend-web/src/components/Common/AboutText.tsx
@@ -5,19 +5,20 @@ interface TextBoxProps {
   height?: string;
   justifyContent?: string;
   fontSize?: string;
-  fontWeight?: string;
+  fontWeight?: string | number;
+  marginL?: string;
 }
 
 export const TextBox = styled.div<TextBoxProps>`
-  border: 1px solid black;
+  // border: 1px solid black;
   box-sizing: border-box;
   display: flex;
-  justify-content: start;
+  justify-content: ${(props) =>
+    props.justifyContent ? props.justifyContent : "start"};
   align-items: center;
-  margin-left: 5%;
+  margin-left: ${(props) => (props.marginL ? props.marginL : "7%")};
   width: ${(props) => (props.width ? props.width : "100%")};
   height: ${(props) => (props.height ? props.height : "50%")};
-  font-size: ${(props) =>
-    props.fontSize ? props.fontSize : "1.7em"}; // <-- Added semicolon here
+  font-size: ${(props) => (props.fontSize ? props.fontSize : "1.7em")};
   font-weight: ${(props) => (props.fontWeight ? props.fontWeight : "bold")};
 `;
