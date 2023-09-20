e987688ce01520f09986ad0b88f41063d99fed2e
aldms8960 <45choi22@gmail.com>
2023-09-20 12:06:55 +0900

    Fix: static_files 설정 수정

--- admin/mwonimoney/settings.py
+++ admin/mwonimoney/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = mySECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'ko'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'Asia/Seoul'
 
@@ -117,13 +117,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-# STATIC_DIR = os.path.join(BASE_DIR,'static')
-# STATICFILES_DIRS = [
-#     STATIC_DIR,
-# ]
+# STATIC_URL = '/static/'
+# # STATIC_DIR = os.path.join(BASE_DIR,'static')
+# # STATICFILES_DIRS = [
+# #     STATIC_DIR,
+# # ]
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+
+STATIC_URL = 'data/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static_files/files/static/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static_files"),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
