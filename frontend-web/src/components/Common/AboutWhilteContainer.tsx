f2965d52f361c01a71c9994b4b881a382940acb7
박기택 <taekputer@gmail.com>
2023-09-20 16:53:11 +0900

    [S09P22B310-178] feat : myPage css 끝!

--- frontend-web/src/components/Common/AboutWhilteContainer.tsx
+++ frontend-web/src/components/Common/AboutWhilteContainer.tsx
@@ -6,7 +6,7 @@ interface WhiteBoxProps {
 
 export const WhiteBox1 = styled.div<WhiteBoxProps>`
   // border: 1px solid black;
-  box-sizing: border-box;
+  // box-sizing: border-box;
   background-color: #ffffff;
   border-radius: 8px;
   width: 90%;
