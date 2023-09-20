2157a99056828aae9f131730d113bc1a87609f66
박기택 <taekputer@gmail.com>
2023-09-20 17:21:10 +0900

    [S09P22B310-178] feat : myPage css v2!

--- frontend-web/src/pages/MyPage.tsx
+++ frontend-web/src/pages/MyPage.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import Switch from "@mui/material/Switch";
-
 import {
   Emoji,
   MainContainer,
@@ -27,8 +26,8 @@ export const Container = styled.div<ContainerProps>`
     props.flexDirection ? props.flexDirection : "row"};
   justify-content: center;
   align-items: center;
-  // border: 1px solid black;
-  // box-sizing: border-box;
+  border: 1px solid black;
+  box-sizing: border-box;
 `;
 
 interface HalfBoxProps {
@@ -37,8 +36,8 @@ interface HalfBoxProps {
 }
 
 const HalfBox = styled.div<HalfBoxProps>`
-  // border: 1px solid black;
-  // box-sizing: border-box;
+  border: 1px solid black;
+  box-sizing: border-box;
   width: ${(props) => (props.width ? props.width : "50%")};
   height: ${(props) => (props.height ? props.height : "100%")};
 `;
@@ -63,37 +62,56 @@ function Main() {
         </TextEmojiBox>
       </TextContainer>
       <Container height="50%">
-        <WhiteBox1>
-          <HalfBox width="70%" height="50%">
+        <WhiteBox1
+          style={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <HalfBox
+            width="70%"
+            height="30%"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
             <TextBox fontSize="1.5em">이름</TextBox>
             <TextBox
               height="0%"
               fontSize="1em"
               fontWeight="normal"
               style={{
-                textDecoration: "underline",
+                marginTop: "10px", // 상단 여백 추가
+                color: "var(--text-color-unactive, #969696)",
               }}
             >
               이지현
             </TextBox>
+          </HalfBox>
+          <HalfBox width="70%" height="30%">
             <TextBox fontSize="1.5em">생년월일</TextBox>
             <TextBox
               height="0%"
               fontSize="1em"
               fontWeight="normal"
               style={{
-                textDecoration: "underline",
+                marginTop: "10px", // 상단 여백 추가
+                color: "var(--text-color-unactive, #969696)",
               }}
             >
               이지현
             </TextBox>
+          </HalfBox>
+          <HalfBox width="70%" height="30%">
             <TextBox fontSize="1.5em">계좌</TextBox>
             <TextBox
               height="0%"
               fontSize="1em"
               fontWeight="normal"
               style={{
-                textDecoration: "underline",
+                marginTop: "10px", // 상단 여백 추가
+                color: "var(--text-color-unactive, #969696)",
               }}
             >
               이지현
@@ -103,7 +121,7 @@ function Main() {
       </Container>
       <Container height="80%">
         <WhiteBox1>
-          <HalfBox width="70%" height="50%">
+          <HalfBox width="90%" height="50%">
             <TextBox fontSize="1.5em">알림설정</TextBox>
             <TextBox
               height="20%"
