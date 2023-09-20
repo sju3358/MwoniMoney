2b96cff919f6fa0fd31edfe6e3049bfa5c7097cd
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-09-20 17:40:42 +0900

    [S09P22B310-190] design: 자식페이지 챌린지 css 및 AboutText에 Text추가

--- frontend-web/src/components/Common/AboutText.tsx
+++ frontend-web/src/components/Common/AboutText.tsx
@@ -21,3 +21,23 @@ export const TextBox = styled.div<TextBoxProps>`
     props.fontSize ? props.fontSize : "1.7em"}; // <-- Added semicolon here
   font-weight: ${(props) => (props.fontWeight ? props.fontWeight : "bold")};
 `;
+
+interface TextProps {
+  color?: string | null;
+  fontfamily?: string | null;
+  fontsize?: string | null;
+  fontstyle?: string | null;
+  fontweight?: string | null;
+  margin?: string | null;
+  padding?: string | null;
+}
+export const Text = styled.div<TextProps>`
+  // border: 1px solid blue;
+  color: ${(props) => (props.color ? props.color : "black")};
+  font-family: ${(props) => (props.fontfamily ? props.fontfamily : "Inter")};
+  font-size: ${(props) => (props.fontsize ? props.fontsize : "1.25rem")};
+  font-style: ${(props) => (props.fontstyle ? props.fontstyle : "normal")};
+  font-weight: ${(props) => (props.fontweight ? props.fontweight : "400")};
+  margin: ${(props) => (props.margin ? props.margin : "0%")};
+  padding: ${(props) => (props.padding ? props.padding : "0%")};
+`;
