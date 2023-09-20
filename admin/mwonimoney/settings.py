fef2b25e92530295e65cb2df2ddb73bb13265afa
aldms8960 <45choi22@gmail.com>
2023-09-20 09:57:21 +0900

    Fix: static_files 설정 수정

--- admin/mwonimoney/settings.py
+++ admin/mwonimoney/settings.py
@@ -16,7 +16,6 @@ from .my_settings import mySECRET_KEY, myDATABASES
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -119,10 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIFS = [
-    BASE_DIR / 'static',
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
 ]
-STATIC_ROOT = os.path.join("staticfiles")
+
+STATIC_ROOT = os.path.join(BASE_DIR,'.static_root')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
