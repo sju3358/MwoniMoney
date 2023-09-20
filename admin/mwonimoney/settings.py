fea33602df4cf116816be855356c3b29e62aef66
aldms8960 <45choi22@gmail.com>
2023-09-20 09:33:12 +0900

    Fix: static_files 설정 수정

--- admin/mwonimoney/settings.py
+++ admin/mwonimoney/settings.py
@@ -119,11 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
-STATIC_ROOT = os.path.join(BASE_DIR, '.static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 
 # Default primary key field type
