b6ab7fb2f1b5e7f746d27aebafc3d7e2a5a248e3
Jaei <dobbykim0320@gmail.com>
2023-09-21 15:06:33 +0900

    [S09P22B310-210] Comment : 필요없는 import 삭제

--- frontend-web/src/modal/Quiz/Success.tsx
+++ frontend-web/src/modal/Quiz/Success.tsx
@@ -1,6 +1,4 @@
 import * as React from "react";
-import Button from "@mui/material/Button";
-import Modal from "@mui/material/Modal";
 import ModalBody from "../ModalBack";
 
 export default function BasicModal() {
