e5b9d85746e58246e518de4f7d4fc1ca04e16382
Jaei <dobbykim0320@gmail.com>
2023-09-21 15:20:06 +0900

    [S09P22B310-215] Fix : modal 사용 편의성 보안2

--- frontend-web/src/modal/Quiz/Success.tsx
+++ frontend-web/src/modal/Quiz/Success.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import ModalBody from "../ModalBack";
+import ChallengeList from "../../components/Parents/Challenge/ChallengeList";
 
 export default function BasicModal() {
   return (
@@ -7,7 +8,7 @@ export default function BasicModal() {
       <ModalBody
         modal_start_text="모달띄우는 버튼에 적을 말"
         modal_title="모달 제목"
-        modal_content="모달 내용"
+        modal_content={<ChallengeList />}
         modal_btn="모달 닫는 버튼"
       />
     </div>
