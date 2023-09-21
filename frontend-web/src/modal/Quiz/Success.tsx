8faef51abcdeef8ec91062112473db8efd3bfdfd
Jaei <dobbykim0320@gmail.com>
2023-09-21 16:51:23 +0900

    [S09P22B310] Fix : merge error

--- frontend-web/src/modal/Quiz/Success.tsx
+++ frontend-web/src/modal/Quiz/Success.tsx
@@ -95,15 +95,9 @@ export default function BasicModal() {
     <div>
       <ModalBody
         modal_start_text="모달띄우는 버튼에 적을 말"
-<<<<<<< frontend-web/src/modal/Quiz/Success.tsx
-        modal_title="모달 제목"
-        modal_content={<ChallengeList />} /**테그 넣는 방법*/
-        modal_btn="모달 닫는 버튼"
-=======
         modal_title=""
         modal_content={<Main />}
         modal_btn="보상받기"
->>>>>>> frontend-web/src/modal/Quiz/Success.tsx
       />
     </div>
   );
