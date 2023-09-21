6b11a43c70d50f13785c71b8c29ae1b0a01d4c01
Jaei <dobbykim0320@gmail.com>
2023-09-21 15:03:18 +0900

    [S09P22B310-210] Desgin : 모달form 생성

--- frontend-web/src/components/Common/Button.tsx
+++ frontend-web/src/components/Common/Button.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 interface BtnProps {
   width?: string;
   height?: string;
-  backColor?: string;
+  backcolor?: string;
   fontSize?: string;
 }
 
@@ -16,7 +16,7 @@ const Btn = styled.div<BtnProps>`
   height: ${(props) => (props.height ? props.height : "50%")};
   font-size: ${(props) => (props.fontSize ? props.fontSize : "1.7em")};
   background-color: ${(props) =>
-    props.backColor ? props.backColor : "#fbd56e"};
+    props.backcolor ? props.backcolor : "#fbd56e"};
   border-radius: 10px;
   font-weight: bold;
 `;
@@ -25,16 +25,16 @@ interface ButtonProps {
   content: string;
   width?: string;
   height?: string;
-  backColor?: string;
+  backcolor?: string;
   fontSize?: string;
 }
 
-function Button({ content, width, height, backColor, fontSize }: ButtonProps) {
+function Button({ content, width, height, backcolor, fontSize }: ButtonProps) {
   return (
     <Btn
       width={width}
       height={height}
-      backColor={backColor}
+      backcolor={backcolor}
       fontSize={fontSize}
     >
       {content}
