a59cccbd8f1ba4e8d0c187fcecdf87c0cd94ae94
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-09-21 16:48:17 +0900

    [S09P22B310-202] design: balance 페이지 css

--- frontend-web/src/components/Common/AboutWhilteContainer.tsx
+++ frontend-web/src/components/Common/AboutWhilteContainer.tsx
@@ -21,3 +21,20 @@ export const WhiteBox1 = styled.div<WhiteBoxProps>`
     props.flexDirection ? props.flexDirection : "row"};
   margin-top: ${(props) => (props.marginB ? props.marginB : "0%")};
 `;
+
+interface WhiteBoxProps_v1 {
+  height?: string | null;
+  width?: string | null;
+  padding?: string | null;
+  margin?: string | null;
+}
+export const WhiteBox = styled.div<WhiteBoxProps_v1>`
+  // border: 1px solid black;
+  border-radius: 8px;
+  width: ${(props) => (props.width ? props.width : "90%")};
+  height: ${(props) => (props.height ? props.height : "90%")};
+  background-color: #ffffff;
+  padding: ${(props) => (props.padding ? props.padding : "5% 0% 0% 0%")};
+  box-sizing: border-box;
+  margin: ${(props) => (props.margin ? props.margin : "0%")};
+`;
