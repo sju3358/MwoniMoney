0897b7a01ce62fca317ff78a488b6f89e2d88bf7
aldms8960 <45choi22@gmail.com>
2023-09-22 14:30:43 +0900

    Feat: Balance, Challenge, Quiz 모델 관리

--- admin/mwonimoney/models.py
+++ admin/mwonimoney/models.py
@@ -1,11 +1,56 @@
 from django.db import models
 
-class Test(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=2)
+class Balance(models.Model):
+    BALANCE_STATUS = (
+        ('WAIT', 'WAIT'),
+        ('RUNNING', 'RUNNING'),
+        ('END','RUNNING')
+    )
+
+    idx = models.AutoField(primary_key=True)
+
+    balance_status = models.CharField(
+        max_length=255,
+        choices=BALANCE_STATUS,
+    )
+    create_time = models.DateTimeField(auto_now_add = True)
+    balance_left_answer = models.CharField(max_length=255)
+    balance_question =  models.CharField(max_length=255)
+    balance_right_answer = models.CharField(max_length=255)
+
     def __str__(self):
-        return str(self.id)+self.name
+        return self.balance_question
 
     class Meta:
-        db_table = 'test'
+        db_table = 'balance'
+        # managed = False
+
+
+class Challenge(models.Model):
+    challenge_idx = models.AutoField(primary_key=True)
+    challenge_category = models.CharField(max_length=255)
+    challenge_title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'[{self.challenge_category}] {self.challenge_title}]'
+
+    class Meta:
+        db_table = 'challenge'
+        # managed = False
+
+class Quiz(models.Model):
+    quiz_idx = models.AutoField(primary_key=True)
+    quiz_question = models.CharField(max_length=255)
+    quiz_answer = models.CharField(max_length=255)
+    quiz_option1 = models.CharField(max_length=255)
+    quiz_option2 = models.CharField(max_length=255)
+    quiz_option3 = models.CharField(max_length=255)
+    quiz_option4 = models.CharField(max_length=255)
+
+
+    def __str__(self):
+        return f'[{self.quiz_question}] {self.quiz_answer}]'
+
+    class Meta:
+        db_table = 'quiz'
         # managed = False
--- admin/mwonimoney/my_settings.py
+++ admin/mwonimoney/my_settings.py
@@ -3,7 +3,7 @@ mySECRET_KEY = 'django-insecure-vce0jr_sl(8t84axzii-zi+!yigles=up!9et52=h02yv4a0
 myDATABASES = {
 	'default' : {
 					'ENGINE' : 'django.db.backends.mysql', # 벡엔드 엔진
-					'NAME' : 'wannabedb', # 'mysql'의 이름을 가진 데이터베이스
+					'NAME' : 'mwonimoneydb', # 'mysql'의 이름을 가진 데이터베이스
 					'USER' : 'wannabe', # 계정
 					'PASSWORD' : 'i9b310p_kplyjc', #rootpassword로 지정할 숫자(6번에 나와있음)
 					'HOST' : 'j9b310.p.ssafy.io',
