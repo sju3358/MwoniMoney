3b22a3f2f879a56df0feaf31c7a0dc9fc20725ac
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-09-22 16:40:14 +0900

    [S09P22B310-191]design:짜금통 css

--- frontend-web/src/components/Common/AboutWhilteContainer.tsx
+++ frontend-web/src/components/Common/AboutWhilteContainer.tsx
@@ -27,14 +27,26 @@ interface WhiteBoxProps_v1 {
   width?: string | null;
   padding?: string | null;
   margin?: string | null;
+  borderradius?: string | null;
+  display?: string | null;
+  justifycontent?: string | null;
+  alignitems?: string | null;
+  flexdirection?: string | null;
 }
 export const WhiteBox = styled.div<WhiteBoxProps_v1>`
   // border: 1px solid black;
-  border-radius: 8px;
+  border-radius: ${(props) =>
+    props.borderradius ? props.borderradius : "8px"};
   width: ${(props) => (props.width ? props.width : "90%")};
   height: ${(props) => (props.height ? props.height : "90%")};
   background-color: #ffffff;
-  padding: ${(props) => (props.padding ? props.padding : "5% 0% 0% 0%")};
+  padding: ${(props) => (props.padding ? props.padding : "0%")};
   box-sizing: border-box;
   margin: ${(props) => (props.margin ? props.margin : "0%")};
+  display: ${(props) => (props.display ? props.display : "")};
+  justify-content: ${(props) =>
+    props.justifycontent ? props.justifycontent : ""};
+  align-items: ${(props) => (props.alignitems ? props.alignitems : "")};
+  flex-direction: ${(props) =>
+    props.flexdirection ? props.flexdirection : ""};
 `;
