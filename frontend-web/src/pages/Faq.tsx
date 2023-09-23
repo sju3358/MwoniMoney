c6c089c49664efcbd2033e722e7ad208817e82c7
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-09-23 16:07:58 +0900

    [S09P22B310-185] design: faq css

--- frontend-web/src/pages/Faq.tsx
+++ frontend-web/src/pages/Faq.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import CustomizedAccordions from "../components/Common/Faq/CustomizedAccordions";
+import { Container } from "../components/Common/About/AboutContainer";
+import { TextBox, Text } from "../components/Common/About/AboutText";
+import { Img } from "../components/Common/About/AboutEmogi";
+import RightArrow from "../assests/image/main/RightArrow.png";
+import { MainContainer } from "../components/Common/Main/Main";
+import { Link } from "react-router-dom";
 
 function Faq() {
-  return <>faqgg</>;
+  return (
+    <Container width="100%" height="100%">
+      <Container
+        width="90%"
+        height="100%"
+        flexDirection="column"
+        justifyContent="flex-start"
+      >
+        <TextBox width="100%" height="10%" marginL="0%">
+          <Link to="/MyPage">
+            <Img src={`${RightArrow}`} width="100%" height="100%" />
+          </Link>
+          <Text fontsize="1.5rem" fontweight="700" padding="0% 0% 0% 30%">
+            FAQ
+          </Text>
+        </TextBox>
+        <Container
+          width="100%"
+          height="100%"
+          style={{ alignItems: "flex-start", padding: "10% 0% 0% 0%" }}
+        >
+          <CustomizedAccordions />
+        </Container>
+      </Container>
+    </Container>
+  );
 }
 
 export default Faq;
