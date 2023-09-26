5040641d726722986d206fa945678f15cec31164
sju3358 <tjdfkr011.dev@gmail.com>
2023-09-26 14:48:07 +0900

    [S09P22B310-000] fix: swagger cors 오류 수정 기본값 https로 변경

--- backend/src/main/java/com/ntt/mwonimoney/WannabeeApplication.java
+++ backend/src/main/java/com/ntt/mwonimoney/WannabeeApplication.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @EnableJpaAuditing
 @EnableScheduling
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "https://j9b310.p.ssafy.io", description = "Default Server Url")})
 public class WannabeeApplication {
 
 	public static void main(String[] args) {
