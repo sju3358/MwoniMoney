87ae0f10aa3ba5ccdb96862ac270a15c6bcdd949
sju3358 <tjdfkr011.dev@gmail.com>
2023-09-26 10:29:42 +0900

    [S09P22B310-247] feat : 워드클라우드 기능 구현

--- backend/src/main/java/com/ntt/mwonimoney/domain/game/service/WordCloudServiceImpl.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/game/service/WordCloudServiceImpl.java
@@ -1,12 +1,19 @@
 package com.ntt.mwonimoney.domain.game.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.ntt.mwonimoney.domain.game.entity.Chat;
 import com.ntt.mwonimoney.domain.game.entity.Word;
 import com.ntt.mwonimoney.domain.game.repository.WordCloudRepository;
+
+import kr.co.shineware.nlp.komoran.core.Komoran;
+import kr.co.shineware.nlp.komoran.model.Token;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 @Service
@@ -14,19 +21,35 @@ import reactor.core.scheduler.Schedulers;
 @Slf4j
 public class WordCloudServiceImpl implements WordCloudService {
 
-    private final WordCloudRepository wordCloudRepository;
+	private final WordCloudRepository wordCloudRepository;
+	private final Komoran komoran;
 
-    @Override
-    public Flux<Word> getWordCloudData(Long balanceGameIdx) {
-        return wordCloudRepository.mFindByBalanceGameIdx(balanceGameIdx)
-                .subscribeOn(Schedulers.boundedElastic());
-    }
+	@Override
+	public Flux<List<Word>> getWordCloudData(Long balanceGameIdx) {
+		return wordCloudRepository.findWordsByBalanceGameIdx(balanceGameIdx)
+			.subscribeOn(Schedulers.boundedElastic());
+	}
 
-    @Override
-    public Mono<Word> addWord(Word word, Long balanceGameIdx) {
+	@Override
+	public void addWord(Chat chat, Long balanceGameIdx) {
 
-        word.setBalanceGameIdx(balanceGameIdx);
+		List<Token> tokens = komoran.analyze(chat.getMessage()).getTokenList();
 
-        return wordCloudRepository.save(word);
-    }
+		List<Word> words = new ArrayList<>();
+		for (Token token : tokens) {
+			String pos = token.getPos();
+			if (pos.startsWith("NN")) {
+				Word word = wordCloudRepository.findWordByWord(token.getMorph())
+					.orElse(Word.builder()
+						.word(token.getMorph())
+						.count(0L)
+						.balanceGameIdx(balanceGameIdx)
+						.build());
+				word.setCount(word.getCount() + 1);
+				words.add(word);
+			}
+		}
+		
+		wordCloudRepository.saveAll(words);
+	}
 }
