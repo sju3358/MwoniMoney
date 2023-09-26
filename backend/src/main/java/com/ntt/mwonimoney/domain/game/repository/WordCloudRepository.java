11575edc59505d5696623de36d8d6934d3b1d6d0
sju3358 <tjdfkr011.dev@gmail.com>
2023-09-26 12:21:14 +0900

    [S09P22B310-247] feat : 워드클라우드 기능 구현

--- backend/src/main/java/com/ntt/mwonimoney/domain/game/repository/WordCloudRepository.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/game/repository/WordCloudRepository.java
@@ -1,21 +1,13 @@
 package com.ntt.mwonimoney.domain.game.repository;
 
-import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.data.mongodb.repository.Tailable;
 
 import com.ntt.mwonimoney.domain.game.entity.Word;
 
-import reactor.core.publisher.Flux;
-
-public interface WordCloudRepository extends ReactiveMongoRepository<Word, String> {
-
-	@Tailable // 커서를 안닫고 계속 유지한다.
-	@Query("{balanceGameIdx: ?0}")
-	Flux<List<Word>> findWordsByBalanceGameIdx(Long balanceGameIdx);
+public interface WordCloudRepository extends MongoRepository<Word, String> {
 
 	@Query("{word : ?0}")
 	Optional<Word> findWordByWord(String word);
