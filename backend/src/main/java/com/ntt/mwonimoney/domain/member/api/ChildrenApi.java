cdbf44310162f26640de29232e1576f3070a21b5
sju3358 <tjdfkr011.dev@gmail.com>
2023-09-30 19:52:37 +0900

    [S09P22B310-000] feat : 멤버 서비스 로그 추가

--- backend/src/main/java/com/ntt/mwonimoney/domain/member/api/ChildrenApi.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/member/api/ChildrenApi.java
@@ -1,5 +1,6 @@
 package com.ntt.mwonimoney.domain.member.api;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
@@ -31,9 +32,13 @@ public class ChildrenApi {
 	public ResponseEntity<List<ChildDto>> getChildrenListRequest(
 		@RequestHeader("Authorization") String accessToken) {
 
+		log.info("[자녀 정보목록 조회 요청 시작]", LocalDateTime.now());
+
 		String memberUUID = jwtTokenProvider.getMemberUUID(accessToken);
 
 		List<ChildDto> childDtoList = childrenService.getChildren(memberUUID);
+
+		log.info("[자녀 정보목록 조회 요청 끝]", LocalDateTime.now());
 
 		return ResponseEntity.ok().body(childDtoList);
 	}
@@ -43,9 +48,13 @@ public class ChildrenApi {
 		@RequestHeader("Authorization") String accessToken,
 		@PathVariable("child_uuid") String childUUID) {
 
+		log.info("[자녀 정보 조회 요청 시작]", LocalDateTime.now());
+
 		String memberUUID = jwtTokenProvider.getMemberUUID(accessToken);
 
 		ChildDto childDto = childrenService.getChildInfo(memberUUID, childUUID);
+
+		log.info("[자녀 정보 조회 요청 끝]", LocalDateTime.now());
 
 		return ResponseEntity.ok().body(childDto);
 	}
@@ -55,9 +64,13 @@ public class ChildrenApi {
 		@RequestHeader("Authorization") String accessToken,
 		@PathVariable String parentUUID) {
 
+		log.info("[자녀 추가 요청 시작]", LocalDateTime.now());
+
 		String childUUID = jwtTokenProvider.getMemberUUID(accessToken);
 
 		childrenService.addParent(parentUUID, childUUID);
+
+		log.info("[자녀 추가 요청 끝]", LocalDateTime.now());
 
 		return ResponseEntity.ok().build();
 	}
@@ -67,9 +80,13 @@ public class ChildrenApi {
 		@RequestHeader("Authorization") String accessToken,
 		@PathVariable("child_uuid") String childUUID) {
 
+		log.info("[자녀 삭제 요청 시작]", LocalDateTime.now());
+
 		String memberUUID = jwtTokenProvider.getMemberUUID(accessToken);
 
 		childrenService.removeChild(memberUUID, childUUID);
+
+		log.info("[자녀 삭제 요청 끝]", LocalDateTime.now());
 
 		return ResponseEntity.ok().build();
 	}
