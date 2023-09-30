9e57f7b35f18deaa156c4c9c3012124f6693c8e0
윤민재 <w124564@naver.com>
2023-09-30 21:00:36 +0900

    [S09P22B310-255] fix : url 변경

--- admin/mwonimoney/urls.py
+++ admin/mwonimoney/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/chatbot/', include('chatbot.urls'))
+    path('django/chatbot/', include('chatbot.urls'))
 ]
