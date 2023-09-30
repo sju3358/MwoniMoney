66850999b9c455632d17c1a424c035dcdf5b3b53
tjdfkr011 <tjdfkr011@naver.com>
2023-10-01 07:00:49 +0900

    [S09P22B310-000] feat : 워드클라우드 기능 구현

--- backend/src/main/java/com/ntt/mwonimoney/domain/game/repository/WordCloudRepository.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/game/repository/WordCloudRepository.java
@@ -1,14 +1,15 @@
 package com.ntt.mwonimoney.domain.game.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 
 import com.ntt.mwonimoney.domain.game.entity.Word;
 
 public interface WordCloudRepository extends MongoRepository<Word, String> {
 
-	@Query("{word : ?0}")
+	List<Word> findWordsByBalanceGameIdx(Long balanceGameIdx);
+
 	Optional<Word> findWordByWord(String word);
 }
