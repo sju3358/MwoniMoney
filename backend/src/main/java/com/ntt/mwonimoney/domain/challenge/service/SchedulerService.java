97d59ca0ce73706bc4c3874169aaba163f3fc17f
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-10-02 01:42:56 +0900

    [S09P22B310-000]fix : MemberChallenge 스케줄러 service 수정

--- backend/src/main/java/com/ntt/mwonimoney/domain/challenge/service/SchedulerService.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/challenge/service/SchedulerService.java
@@ -1,6 +1,7 @@
 package com.ntt.mwonimoney.domain.challenge.service;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,10 +12,12 @@ import com.ntt.mwonimoney.domain.challenge.entity.MemberChallenge;
 import com.ntt.mwonimoney.domain.challenge.repository.MemberChallengeRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class SchedulerService {
 	private final MemberChallengeRepository memberChallengeRepository;
 
@@ -22,13 +25,16 @@ public class SchedulerService {
 	@Scheduled(cron = "0 0 0 * * *")
 	public void run() {
 		LocalDate now = LocalDate.now();
+		log.info("현재 시간 : {}", now);
 
 		// 챌린지의 status값이 3인 것을 모두 찾고
 		// 그 챌린지에 대해 endDate + 1이 now()와 같을 때,
 		// update를 한다.
 		List<MemberChallenge>
 			expirationChallengeList = memberChallengeRepository.findByEndTimeBeforeAndStatus(
-			now.atStartOfDay(), 0);
+			now.atStartOfDay(), Arrays.asList(0, 1, 2));
+		log.info("시간 : {}", now.atStartOfDay());
+		log.info("리스트 : {}", expirationChallengeList);
 
 		for (MemberChallenge memberChallenge :
 			expirationChallengeList) {
