e6d050e9a7bbd9352a4838c22bfc97c4057020d3
aldms8960 <45choi22@gmail.com>
2023-10-03 23:54:22 +0900

    Fix: small-account 저장 수정

--- backend/src/main/java/com/ntt/mwonimoney/domain/account/repository/FinAccountRepositoryCustom.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/repository/FinAccountRepositoryCustom.java
@@ -1,12 +1,15 @@
 package com.ntt.mwonimoney.domain.account.repository;
 
 import com.ntt.mwonimoney.domain.account.entity.FinAccount;
+import com.ntt.mwonimoney.domain.account.entity.FinAccountStatus;
 import com.ntt.mwonimoney.domain.account.entity.FinAccountType;
 import com.ntt.mwonimoney.domain.member.model.vo.SmallAccount;
+import jakarta.transaction.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public interface FinAccountRepositoryCustom {
-    public Optional<FinAccount> findFinAccountByMemberAndType(Long memberIdx, FinAccountType finAccountType);
+    public Optional<FinAccount> findFinAccountByMemberAndTypeAndStatus(Long memberIdx, FinAccountType finAccountType, FinAccountStatus finAccountStatus);
 }
