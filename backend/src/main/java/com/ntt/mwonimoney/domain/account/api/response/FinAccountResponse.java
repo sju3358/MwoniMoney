1dd17f457f7128feff02b385241364589f7a0eda
윤민재 <w124564@naver.com>
2023-10-04 01:37:33 +0900

    [S09P22B310-000] feat : accounts 조회 기능 추가

---
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/api/response/FinAccountResponse.java
@@ -0,0 +1,21 @@
+package com.ntt.mwonimoney.domain.account.api.response;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ntt.mwonimoney.domain.account.model.dto.FinAccountTransactionDto;
+
+import lombok.Data;
+
+@Data
+public class FinAccountResponse {
+	private String number;
+	private Long remain;
+	private LocalDate createdDay;
+	private List<FinAccountTransactionDto> finAccountTransactionDtos;
+
+	public FinAccountResponse() {
+		finAccountTransactionDtos = new ArrayList<>();
+	}
+}
--- backend/src/main/java/com/ntt/mwonimoney/domain/account/repository/FinAccountTransactionRepository.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/repository/FinAccountTransactionRepository.java
@@ -1,20 +1,22 @@
 package com.ntt.mwonimoney.domain.account.repository;
 
-import com.ntt.mwonimoney.domain.account.entity.FinAccount;
-import com.ntt.mwonimoney.domain.account.entity.FinAccountTransaction;
-import jakarta.transaction.Transactional;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.ntt.mwonimoney.domain.account.entity.FinAccountTransaction;
+
+import jakarta.transaction.Transactional;
+
 @Transactional
-public interface FinAccountTransactionRepository extends JpaRepository<FinAccountTransaction, Long> {
+public interface FinAccountTransactionRepository
+	extends JpaRepository<FinAccountTransaction, Long>, FinAccountTransactionRepositoryCustom {
 
-    Slice<FinAccountTransaction> findByFinAccountIdx(Long finAccountIdx, Pageable pageable);
+	Slice<FinAccountTransaction> findByFinAccountIdx(Long finAccountIdx, Pageable pageable);
 
-    Slice<FinAccountTransaction> findByFinAccountIdxAndMoneyGreaterThanEqual(Long finAccountIdx, int money, Pageable pageable);
+	Slice<FinAccountTransaction> findByFinAccountIdxAndMoneyGreaterThanEqual(Long finAccountIdx, int money,
+		Pageable pageable);
 
-    Slice<FinAccountTransaction> findByFinAccountIdxAndMoneyLessThanEqual(Long finAccountIdx, int money, Pageable pageable);
-
+	Slice<FinAccountTransaction> findByFinAccountIdxAndMoneyLessThanEqual(Long finAccountIdx, int money,
+		Pageable pageable);
 }
