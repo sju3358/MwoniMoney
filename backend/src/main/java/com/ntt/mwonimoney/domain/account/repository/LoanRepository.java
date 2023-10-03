0a86dfb58b0acf0e355d8f8a638d5afc86c00900
aldms8960 <45choi22@gmail.com>
2023-10-03 22:03:03 +0900

    Fix: loan 수정

--- backend/src/main/java/com/ntt/mwonimoney/domain/account/repository/LoanRepository.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/repository/LoanRepository.java
@@ -2,10 +2,12 @@ package com.ntt.mwonimoney.domain.account.repository;
 
 import com.ntt.mwonimoney.domain.account.entity.Loan;
 import com.ntt.mwonimoney.domain.account.entity.LoanStatus;
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Transactional
 public interface LoanRepository extends JpaRepository<Loan, Long>, LoanRepositoryCustom{
     Slice<Loan> findByStatusAndLender(LoanStatus status, Long lender, Pageable pageable);
     Slice<Loan> findByStatusAndBorrower(LoanStatus status, Long borrower, Pageable pageable);
