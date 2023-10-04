c545168047a443a806321d38f604ba9c9ef636a3
Jaei <dobbykim0320@gmail.com>
2023-10-04 12:46:40 +0900

    [S09P22B310-268] Feat : Account Axios 연결

--- frontend-web/src/states/UserInfoState.ts
+++ frontend-web/src/states/UserInfoState.ts
@@ -54,6 +54,8 @@ export const userCheckState = atom<userCheckProps>({
 export interface userAccountProps {
   account: string;
   status: string;
+  remain: number;
+  startDate: string;
 }
 
 export const userAccountState = atom<userAccountProps>({
@@ -61,5 +63,7 @@ export const userAccountState = atom<userAccountProps>({
   default: {
     account: "",
     status: "UNKOWN",
+    remain: 0,
+    startDate: "0000-00-00",
   },
 });
