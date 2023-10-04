9eda07dc09a99138f3e44347e19754cdd83c0ebb
tjdfkr011 <tjdfkr011@naver.com>
2023-10-04 19:51:56 +0900

    [S09P22B310-000] feat : 짜금통 자동 저장 추가

--- backend/src/main/java/com/ntt/mwonimoney/domain/account/service/v2/FinAccountTransactionServiceImplV2.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/service/v2/FinAccountTransactionServiceImplV2.java
@@ -4,19 +4,20 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
-import com.ntt.mwonimoney.domain.account.model.dto.FinAccountTransactionDto2;
 import org.springframework.stereotype.Service;
 
 import com.ntt.mwonimoney.domain.account.entity.FinAccount;
 import com.ntt.mwonimoney.domain.account.entity.FinAccountTransaction;
 import com.ntt.mwonimoney.domain.account.entity.FinAccountType;
+import com.ntt.mwonimoney.domain.account.model.dto.FinAccountTransactionDto2;
 import com.ntt.mwonimoney.domain.account.model.dto.GetTransactionResponseDto;
 import com.ntt.mwonimoney.domain.account.model.dtoV2.GetTransactionRequestDto;
 import com.ntt.mwonimoney.domain.account.repository.FinAccountRepository;
 import com.ntt.mwonimoney.domain.account.repository.FinAccountTransactionRepository;
+import com.ntt.mwonimoney.domain.member.entity.Child;
 import com.ntt.mwonimoney.domain.member.entity.Member;
+import com.ntt.mwonimoney.domain.member.model.vo.MemberRole;
 import com.ntt.mwonimoney.domain.member.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -30,7 +31,8 @@ public class FinAccountTransactionServiceImplV2 {
 	private final MemberRepository memberRepository;
 
 	public GetTransactionResponseDto getTransaction(Long memberIdx, GetTransactionRequestDto getTransactionRequestDto) {
-		List<FinAccountTransaction> result = finAccountTransactionRepository.getTransaction(memberIdx, getTransactionRequestDto);
+		List<FinAccountTransaction> result = finAccountTransactionRepository.getTransaction(memberIdx,
+			getTransactionRequestDto);
 
 		Long totalPlus = 0L;
 		Long totalMinus = 0L;
@@ -50,26 +52,23 @@ public class FinAccountTransactionServiceImplV2 {
 			}
 
 			FinAccountTransactionDto2 responseDto = FinAccountTransactionDto2.builder()
-					.money(money)
-					.balance(balance)
-					.memo(memo)
-					.time(time)
-					.build();
+				.money(money)
+				.balance(balance)
+				.memo(memo)
+				.time(time)
+				.build();
 
 			responseList.add(responseDto);
 		}
 
 		GetTransactionResponseDto result1 = GetTransactionResponseDto.builder()
-				.FinAccountTransactionDto2(responseList)
-				.totalMinus(totalMinus)
-				.totalPlus(totalPlus)
-				.build();
-
+			.FinAccountTransactionDto2(responseList)
+			.totalMinus(totalMinus)
+			.totalPlus(totalPlus)
+			.build();
 
 		return result1;
 	}
-
-
 
 	public void makeTransaction(String fromUUID, String toUUID, int amount, String memo) {
 		makeTransaction(fromUUID, toUUID, amount, memo, FinAccountType.GENERAL, FinAccountType.GENERAL);
@@ -113,6 +112,21 @@ public class FinAccountTransactionServiceImplV2 {
 
 		finAccountTransactionRepository.save(fromTransaction);
 		finAccountTransactionRepository.save(toTransaction);
+
+		if (toMember.getMemberRole() == MemberRole.CHILD
+			&& ((Child)toMember).getSmallAccount() != null
+			&& toType.equals(FinAccountType.GENERAL)) {
+
+			int saveRatio = ((Child)toMember).getSmallAccount().getSaveRatio();
+			int moneyToSaveInSmall = amount * saveRatio / 100;
+			makeTransaction(
+				toUUID,
+				toUUID,
+				moneyToSaveInSmall,
+				"[짜금통 자동 저금] : " + toTransaction.getMemo(),
+				FinAccountType.GENERAL,
+				FinAccountType.SMALL);
+		}
 
 	}
 
