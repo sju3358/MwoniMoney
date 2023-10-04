6b270e6cda1e825fc41455b2b4edcc4420d9548f
sju3358 <tjdfkr011.dev@gmail.com>
2023-10-04 16:11:35 +0900

    [S09P22B310-000] feat : 짜금통 api 추가 (블랙박스)

--- backend/src/main/java/com/ntt/mwonimoney/domain/account/service/SmallAccountService.java
+++ backend/src/main/java/com/ntt/mwonimoney/domain/account/service/SmallAccountService.java
@@ -1,12 +1,10 @@
 package com.ntt.mwonimoney.domain.account.service;
 
-import com.ntt.mwonimoney.domain.account.entity.FinAccount;
 import com.ntt.mwonimoney.domain.member.model.vo.SmallAccount;
 
 public interface SmallAccountService {
 
 	public void closeSmallAccount(Long smallAccountIdx);
 
-	public FinAccount openSmallAccount(Long memberIdx, SmallAccount smallAccount);
-
+	void openSmallAccount(Long memberIdx, SmallAccount smallAccount);
 }
