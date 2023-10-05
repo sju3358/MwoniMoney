eb5608d36f61f26fd8c2f7d2d2102b84b1fd3bdd
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-10-05 15:52:51 +0900

    [S09P22B310-293] design : 버튼 css

--- frontend-web/src/modal/ModalImg.tsx
+++ frontend-web/src/modal/ModalImg.tsx
@@ -62,6 +62,8 @@ export const ModalContent = styled.div<ModalContentProps>`
 `;
 interface Btn {
   back_color?: string;
+  af_back_color?: string;
+  border?: string;
 }
 /**모달 닫기 버튼 */
 export const ModalBtn = styled.div<Btn>`
@@ -70,10 +72,16 @@ export const ModalBtn = styled.div<Btn>`
   height: 80%;
   background-color: ${(props) =>
     props.back_color ? props.back_color : "#fbd570"};
-  border-radius: 10px;
+  border-radius: 15px;
   display: flex;
   justify-content: center;
   align-items: center;
+  border: ${(props) => (props.border ? props.border : "none")};
+  &:active {
+    background-color: ${(props) =>
+      props.af_back_color ? props.af_back_color : props.af_back_color};
+    transform: translate(0em, 0.2em);
+  }
 `;
 
 interface ModalProps {
@@ -118,8 +126,15 @@ const ModalBody3: React.FC<ModalProps> = ({
             {modal_content}
           </ModalContent>
           <ModalTopBottom justify="space-around">
-            <ModalBtn onClick={handleClose}>{modal_btn1}</ModalBtn>
-            <ModalBtn back_color="#f5f3ed" onClick={handleClose}>
+            <ModalBtn af_back_color="#FFC107" onClick={handleClose}>
+              {modal_btn1}
+            </ModalBtn>
+            <ModalBtn
+              back_color="white"
+              af_back_color="#BBBBBB"
+              border="1px solid #BBBBBB"
+              onClick={handleClose}
+            >
               {modal_btn2}
             </ModalBtn>
           </ModalTopBottom>
